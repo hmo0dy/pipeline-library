26a1f94c2bc7e65124ed7725c8006ba2f3fcecae
hmo0dy <ahmed-07007@workshop.cb-sa.io>
2021-07-27 17:30:37 +0000

    updating hmo0dy/pipeline-library configBundleUpdate.groovy for ahmed-07007

--- vars/configBundleUpdate.groovy
+++ vars/configBundleUpdate.groovy
@@ -12,6 +12,15 @@ def call(String nameSpace = "sda") {
         sh "cp *.yaml ${bundleName}"
         sh "kubectl cp --namespace ${nameSpace} ${bundleName} cjoc-0:/var/jenkins_home/jcasc-bundles-store/ -c jenkins"
       }
+      echo "begin config bundle reload"
+      sh "curl -O https://raw.githubusercontent.com/cloudbees-days/ops-workshop-setup/master/groovy/reload-casc.groovy"
+      sh "curl -O http://${bundleName}/${bundleName}/jnlpJars/jenkins-cli.jar"
+      withCredentials([usernamePassword(credentialsId: 'admin-cli-token', usernameVariable: 'JENKINS_CLI_USR', passwordVariable: 'JENKINS_CLI_PSW')]) {
+          sh """
+            alias cli='java -jar jenkins-cli.jar -s http://${bundleName}/${bundleName}/ -auth $JENKINS_CLI_USR:$JENKINS_CLI_PSW'
+            cli groovy =<./reload-casc.groovy
+          """
+      }
     }
   }
 }
